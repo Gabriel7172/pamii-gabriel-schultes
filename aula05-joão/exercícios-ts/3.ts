a535060090f54490a59c9046ac6432c35319758b
Gabriel Schultes <schultesgabrieltony@gmail.com>
2025-06-18 13:36:05 -0300

    atividades João siles

---
+++ aula05-joão/exercícios-ts/3.ts
@@ -0,0 +1,13 @@
+interface Livro{
+    título: string,
+    autor: string,
+    publicação: number
+}
+
+var L1: Livro = {
+    título: "Viscoso mais gostoso",
+    autor: "Timão",
+    publicação: 1883
+}
+
+console.log(L1);
